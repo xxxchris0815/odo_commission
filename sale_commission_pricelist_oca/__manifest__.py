1d83fefd005d5de8d072b922150e95febd7369fd
Borruso <gborruso@dinamicheaziendali.it>
2026-03-24 13:10:33 +0100

    [MOV] sale_commission_pricelist: rename to sale_commission_pricelist_oca

--- sale_commission_pricelist_oca/__manifest__.py
+++ sale_commission_pricelist_oca/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Sales commissions by pricelist",
+    "name": "Sales commissions by pricelist OCA",
     "version": "18.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA), ArcheTI",
     "category": "Sales Management",
