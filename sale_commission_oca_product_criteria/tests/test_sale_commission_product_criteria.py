OCA/commission

[FIX]sale_commission_product_criteria: ignore error on create

sale_commission_product_criteria/tests/test_sale_commission_product_criteria.py
@@ -3,7 +3,7 @@
 
 
 from odoo.exceptions import ValidationError
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import Form, SavepointCase
 
 
 class TestSaleCommission(SavepointCase):
@@ -198,3 +198,19 @@ class TestSaleCommission(SavepointCase):
         invoice = sale_order.invoice_ids - old_invoices
         invoice.flush()
         return invoice
+
+    def test_on_create_check(self):
+        f = Form(self.commission_model)
+        f.name = "New commission type"
+        f.save()
+
+        so = self._create_sale_order(self.product_4, self.partner)
+        self.assertEqual(
+            so.order_line.agent_ids.commission_id, self.rules_commission_id
+        )
+        self.assertEqual(self.rules_commission_id.commission_type, "product")
+
+        so.action_confirm()
+        with self.assertRaises(ValidationError):
+            self.rules_commission_id.commission_type = "fixed"
+            self.rules_commission_id.onchange_commission_type()

xxxchris0815/odo_commission

sale_commission_oca_product_criteria/tests/test_sale_commission_product_criteria.py
@@ -4,7 +4,7 @@
 
 
 from odoo.exceptions import ValidationError
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import Form, TransactionCase
 
 
 class TestSaleCommission(TransactionCase):
@@ -202,3 +202,19 @@ class TestSaleCommission(TransactionCase):
         wizard.create_invoices()
         invoice = sale_order.invoice_ids - old_invoices
         return invoice
+
+    def test_on_create_check(self):
+        f = Form(self.commission_model)
+        f.name = "New commission type"
+        f.save()
+
+        so = self._create_sale_order(self.product_4, self.partner)
+        self.assertEqual(
+            so.order_line.agent_ids.commission_id, self.rules_commission_id
+        )
+        self.assertEqual(self.rules_commission_id.commission_type, "product")
+
+        so.action_confirm()
+        with self.assertRaises(ValidationError):
+            self.rules_commission_id.commission_type = "fixed"
+            self.rules_commission_id.onchange_commission_type()